06fcd8dadf34b07fb32cd9b8c275d62c65348688
memetibryan <memetibryan@gmail.com>
2017-07-05 12:28:17 +0300

    all tests passed

---
+++ lib/Tamagotchi.rb
@@ -0,0 +1,48 @@
+class Tamagotchi	
+
+	attr_reader(:name, :food_level, :sleep_level, :activity_level)
+
+	define_method(:initialize) do |attributes|
+	  @@food_level = 10
+	  @@sleep_level = 10
+	  @@activity_level = 10
+	  @@ame = self
+    end
+
+    def set_food_level
+  		@food_level = @@food_level - 1
+    end
+
+	def is_alive
+		if @@food_level.eql?(10)	
+	    true
+	else
+		false
+		end
+
+		if @@sleep_level.eql?(10)
+		true 
+	else
+		false
+		end
+
+		if @@activity_level.eql?(10)
+		true 
+	else
+		false
+		end
+	end
+
+	def time_passes
+		numb = 0
+
+		while numb < 1
+			@@food_level = @@food_level -=1
+			numb +=1
+		end
+	end
+
+	def food_level
+		@@food_level
+	end
+end
---
+++ spec/tamagotchi_spec.rb
@@ -0,0 +1,36 @@
+require('rspec')
+  require('Tamagotchi')
+
+describe(Tamagotchi) do
+    describe("#tamagotchi")
+      it("sets the name and life levels of a new Tamagotchi") do
+        my_pet = Tamagotchi.new("lil dragon")
+        expect(my_pet.name()).==("lil dragon")
+        expect(my_pet.food_level()).==(10)
+        expect(my_pet.sleep_level()).==(10)
+        expect(my_pet.activity_level()).==(10)
+      end
+    end
+
+    describe("#is_alive") do
+      it("is alive if the food level is above 0") do
+        my_pet = Tamagotchi.new("lil dragon")
+        expect(my_pet.is_alive()).to(eq(true))
+      end
+    end
+
+    describe("#set_food_level") do
+      it("is dead if the food level is 0") do
+        my_pet = Tamagotchi.new("lil dragon")
+        my_pet.set_food_level() #make a method that will change the food level of your tamagotchi.
+        expect(my_pet.is_alive()).==(false)
+      end
+    end
+
+    describe("#time_passes") do
+      it("decreases the amount of food the Tamagotchi has left by 1") do
+        my_pet = Tamagotchi.new("lil dragon")
+        my_pet.time_passes()  #decide what trigger you will use to make time pass
+        expect(my_pet.food_level()).to(eq(9))
+      end
+    end
